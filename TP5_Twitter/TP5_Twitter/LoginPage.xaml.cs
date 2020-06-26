104e7672a283ddb3b73e374a89cc5717c7ff8708
Clément Bricola <clement.bricola@hotmail.fr>
2020-06-26 16:14:26 +0200

    TP6_Twitter

--- TP5_Twitter/TP5_Twitter/LoginPage.xaml.cs
+++ TP5_Twitter/TP5_Twitter/LoginPage.xaml.cs
@@ -1,25 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TP5_Twitter.Modeles;
-using Xamarin.Forms;
 using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace TP5_Twitter
 {
-
-    [DesignTimeVisible(false)]
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
     {
-        public MainPage()
+        public LoginPage()
         {
             InitializeComponent();
-
         }
+
         public void Connection_Clicked(object sender, EventArgs e)
         {
             var current = Connectivity.NetworkAccess;
@@ -31,7 +30,6 @@ namespace TP5_Twitter
                 CacherErreur();
                 if (string.IsNullOrEmpty(this.id.Text.ToString()) || this.id.Text.Length < 3 || string.IsNullOrEmpty(this.pwd.Text.ToString()) || this.pwd.Text.Length < 6)
                 {
-                    CacherPost();
                     this.AfficherErreur("Veuillez saisir un identifiant et un mot de passe valide");
                     return;
                 }
@@ -45,43 +43,20 @@ namespace TP5_Twitter
 
                     bool result = TS.Authenticate(User);
 
-                    if (result == true)
+                    if (result)
                     {
-
-                        AfficherPost();
-                        return;
-
+                        Redirect();
                     }
                     else
                     {
-                        CacherPost();
-                        this.AfficherErreur("Vous ne possédez aucun tweet! ");
-                        return;
+                        this.AfficherErreur("Compte introuvable");
+
                     }
+
+
 
                 }
             }
-            else 
-            {
-
-                CacherPost();
-                this.AfficherErreur("Vous n'êtes pas connecté à internet ! ");
-                return;
-
-
-
-            }
-
-        }
-
-        private void AfficherPost()
-        {
-            this.post.IsVisible = true;
-        }
-
-        private void CacherPost()
-        {
-            this.post.IsVisible = false;
         }
 
         private void AfficherErreur(string erreur)
@@ -90,10 +65,17 @@ namespace TP5_Twitter
             this.erreur.IsVisible = true;
         }
 
+        
+
         private void CacherErreur()
         {
             this.erreur.IsVisible = false;
         }
+
+        async void Redirect()
+        {
+            await Navigation.PushAsync(new TweetPage());
+
+        }
     }
 }
-
---
+++ TP5_Twitter/TP5_Twitter/TweetPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP5_Twitter.Modèles;
+using TP5_Twitter.Modeles;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TP5_Twitter
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TweetPage : ContentPage
+    {
+        public TweetPage()
+        {
+            InitializeComponent();
+
+            TwitterService TwitterService = new TwitterService();
+            this.MaListe.ItemsSource = TwitterService.GetTweets();
+        }
+    }
+}
